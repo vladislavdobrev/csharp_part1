83e28602f932b3c7238d6d3155fac3beb5a0b526
xakepa <vladislavdd@gmail.com>
2013-01-22 16:12:53 +0200

    renewing

--- 2. Data Types And Variables/9. IsoscelesTriangle/IsoscelesTriangle.cs
+++ 2. Data Types And Variables/9. IsoscelesTriangle/IsoscelesTriangle.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Text;
 
 class IsoscelesTriangle
 {
     static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
         char symbol = '©';
         Console.WriteLine("  {0}\n {1}{2}{3}\n{4}{5}{6}{7}{8}\n", 
             symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol, symbol);
